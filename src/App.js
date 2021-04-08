48f8f1299376690b8b6957b9517103d940a70bf2
Yohan Fleury <yfleury95@gmail.com>
2021-04-08 22:59:22 +0200

    new banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { MovieCard } from "./components/MovieCard";
+import { Banner } from "./components/Banner";
 
 function App() {
   return (
     <div>
+      <Banner />
       <MovieCard />
     </div>
   );
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import banner from '../assets/banner.jpg'
+
+export const Banner = () => {
+    return (
+        <div>
+            <img src={banner} alt="banner" />
+        </div>
+    )
+}

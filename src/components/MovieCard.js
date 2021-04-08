0e34b8b79998e46b02349d0b2104721887a6b1fe
Yohan Fleury <yfleury95@gmail.com>
2021-04-08 12:35:17 +0200

    created all cards for all movies from movies.js

---
+++ src/components/MovieCard.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react'
+import { movies$ } from "../movies.js";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+export const MovieCard = () => {
+
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        movies$.then(success => {
+            setMovies(success)
+        })
+    }, [movies])
+    
+    return (
+        <div>
+            <h1> Voici la liste des films </h1>
+            <div>
+                
+                    {movies.map(data => {
+                        return (
+                            <Card style={{ width: '18rem' }} key={data.id}>
+                                <Card.Body>
+                                <Card.Title> {data.title}</Card.Title>
+                                <Card.Text>
+                                    Catégorie
+                                </Card.Text>
+                                <Button variant="success"> Like </Button>
+                                <Card.Text> Nombre de likes </Card.Text>
+                                <Button variant="danger"> Dislike </Button>
+                                <Card.Text> Nombre de dislikes </Card.Text>
+                                <Card.Text> Ici la jauge like/dislike</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        )
+                    })} 
+                
+            </div>
+            
+        </div>
+        
+    )
+}

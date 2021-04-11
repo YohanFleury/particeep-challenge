6f6c4ed82a870c88dd9a194a0e0bc190a1f53fe6
Yohan Fleury <yfleury95@gmail.com>
2021-04-11 23:47:26 +0200

    The reducer is now cleaner

--- src/redux/reducers/reducerCard.js
+++ src/redux/reducers/reducerCard.js
@@ -1,16 +1,9 @@
-import { ADD_DISLIKE, ADD_LIKE, DELETE_CARD, FILTER_CATEGORY } from './constants'
+import { ADD_DISLIKE, ADD_LIKE, DELETE_CARD } from './constants'
 
-// Helpers to make the code cleaner in the reducer
+// Helpers to make the code cleaner inside the reducer
 
-const helperDeleteCard = (state, id) => {
-    const cards = state.filter(card => card.id !== id)
-    return cards
-}
-
-export const reducerCard = (state=[], action) => {
-    switch (action.type) {
-        case ADD_LIKE:
-            const index = state.findIndex(movie => movie.id === action.payload)
+const helperAddLike = (state, id) => {
+    const index = state.findIndex(movie => movie.id === id)
             const mov = state[index];
             mov.likes = mov.likes+1;
             return [
@@ -18,16 +11,35 @@ export const reducerCard = (state=[], action) => {
                 mov,
                 ...state.slice(index+1)
             ]
+}
+
+const helperAddDislike = (state, id) => {
+    const index2 = state.findIndex(movie => movie.id === id)
+    const mov2 = state[index2];
+    mov2.dislikes = mov2.dislikes+1;
+    return [
+        ...state.slice(0, index2),
+        mov2,
+        ...state.slice(index2+1)
+    ]
+}
+
+const helperDeleteCard = (state, id) => {
+    const cards = state.filter(card => card.id !== id)
+    return cards
+}
+
+// Reducer
+
+export const reducerCard = (state, action) => {
+    switch (action.type) {
+        case ADD_LIKE:
+            state = helperAddLike(state, action.payload)
+            return state
 
         case ADD_DISLIKE:
-            const index2 = state.findIndex(movie => movie.id === action.payload)
-            const mov2 = state[index2];
-            mov2.dislikes = mov2.dislikes+1;
-            return [
-                ...state.slice(0, index2),
-                mov2,
-                ...state.slice(index2+1)
-            ]
+            state = helperAddDislike(state, action.payload)
+            return state
 
         case DELETE_CARD:
         state = helperDeleteCard(state, action.payload)
